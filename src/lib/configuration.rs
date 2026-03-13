6b2cd7f88926a350667852b7d189e6de3834b852
Jeff Mitchell <crusty.rustacean@gmail.com>
2026-03-13 06:59:23 -0700

    improvement: remove figment crate, go with config

--- src/lib/configuration.rs
+++ src/lib/configuration.rs
@@ -1,34 +1,58 @@
-// src/lib/configuration.rs
+// src/configuration.rs
 
 // dependencies
-use figment::{
-    Figment,
-    providers::{Env, Format, Yaml},
-};
-use serde::Deserialize;
 use serde_aux::field_attributes::deserialize_number_from_string;
+use std::convert::{TryFrom, TryInto};
 
-// struct type to represent the general settings
-#[derive(Clone, Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct Settings {
     pub application: ApplicationSettings,
 }
 
-// struct type to represent individual Application settings
-#[derive(Clone, Deserialize)]
+#[derive(serde::Deserialize, Clone)]
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
+    pub base_url: String,
 }
 
-// enum type to represent the runtime environment
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // Detect the running environment.
+    // Default to `local` if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+    let settings = config::Config::builder()
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
+
+/// The possible runtime environment for our application.
 pub enum Environment {
     Local,
     Production,
 }
 
-// methods for the Environment type
 impl Environment {
     pub fn as_str(&self) -> &'static str {
         match self {
@@ -38,7 +62,6 @@ impl Environment {
     }
 }
 
-// implement the TryFrom trait for the Environment type
 impl TryFrom<String> for Environment {
     type Error = String;
 
@@ -52,27 +75,4 @@ impl TryFrom<String> for Environment {
             )),
         }
     }
-}
-
-// function to read the configuration from associated files
-pub fn get_configuration() -> Result<Settings, Box<figment::Error>> {
-    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
-    let configuration_directory = base_path.join("configuration");
-
-    let environment: Environment = std::env::var("APP_ENVIRONMENT")
-        .unwrap_or_else(|_| "local".into())
-        .try_into()
-        .expect("Failed to parse APP_ENVIRONMENT");
-
-    let environment_filename = format!("{}.yaml", environment.as_str());
-
-    let settings: Settings = Figment::new()
-        .merge(Yaml::file(configuration_directory.join("base.yaml")))
-        .merge(Yaml::file(
-            configuration_directory.join(environment_filename),
-        ))
-        .merge(Env::prefixed("APP_").split("__"))
-        .extract()?;
-
-    Ok(settings)
 }
